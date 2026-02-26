937a9016c3bc6e018abc7492fc4da3678238a902
Gustavo Gomes Ribeiro Pestana <gustavopestanagr@gmail.com>
2026-02-26 20:28:32 -0300

    modified:   src/@types/express.d.ts

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,10 +1,7 @@
-// src/@types/express.d.ts
 import * as express from 'express';
-
 declare global {
     namespace Express {
         interface Request {
-            // Aqui definimos que TODO 'req' pode ter um objeto user
             user?: {
                 id: number;
             };
